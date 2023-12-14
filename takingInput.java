a75545967ceae1da70c1cdde264a6f4167bc402e
Arnab Dutta <arnabad947@gmail.com>
2023-12-15 02:50:03 +0530

    assignment

---
+++ takingInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class takingInput {
+    public static void main(String[] args){
+        String str;
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        System.out.println(str);
+    }
+}
