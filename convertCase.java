50a9fd2a3cef19bbebe6785480c982bf186c2707
Arnab Dutta <arnabad947@gmail.com>
2023-12-15 02:23:46 +0530

    assignment

---
+++ convertCase.java
@@ -0,0 +1,9 @@
+public class convertCase {
+    public static void main(String[] args) {
+        String upperCase = "HELLO WORLD";
+        String lowerCase = "java";
+
+        System.out.println("String: "+ upperCase + " convert to lowercase: " + upperCase.toLowerCase() );
+        System.out.println("String: "+ lowerCase + " convert to uppercase: " + lowerCase.toUpperCase() );
+    }
+}
