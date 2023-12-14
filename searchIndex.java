bc0ddb46f180d474445a691d1a18cc86e3870d9f
Arnab Dutta <arnabad947@gmail.com>
2023-12-15 02:39:40 +0530

    assignment

---
+++ searchIndex.java
@@ -0,0 +1,7 @@
+public class searchIndex {
+    public static void main(String[] args){
+        String str = "Java Programming";
+        int index = str.indexOf("Programming");
+        System.out.println("Found at index: " + index);
+    }
+}
